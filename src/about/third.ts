f6685a52c08c76d3a8c1c7795eae1fb6d2d1d92a
kika <franfonsecav@gmail.com>
2022-12-19 10:31:01 +0100

    password work

--- src/about/third.ts
+++ src/about/third.ts
@@ -1,30 +1,58 @@
 export {};
+import axios from "axios";
+import { InferCustomEventPayload } from "vite/types/customEvent";
 
 const passwordWeak = document.querySelectorAll(
-  ".p-weak"
+	".p-weak"
 ) as NodeListOf<HTMLElement>;
 
 const passwordMedium = document.querySelectorAll(
-  ".p-medium"
+	".p-medium"
 ) as NodeListOf<HTMLElement>;
 
 const passwordStrong = document.querySelectorAll(
-  ".p-strong"
+	".p-strong"
 ) as NodeListOf<HTMLElement>;
 
 const password = document.querySelectorAll(
-  ".password"
+	".password"
 ) as NodeListOf<HTMLInputElement>;
 
+const form = document.getElementById("wrapper") as HTMLFormElement;
+
 for (let i = 0; i < password.length; i++) {
-  password[i].addEventListener("keydown", () => {
-    switch (true) {
-      case password[i].value.length > 20:
-        passwordStrong[i].style.backgroundColor = "green";
-      case password[i].value.length > 12:
-        passwordMedium[i].style.backgroundColor = "orange";
-      case password[i].value.length > 3:
-        passwordWeak[i].style.backgroundColor = "red";
-    }
-  });
+	password[i].addEventListener("keydown", () => {
+		switch (true) {
+			case password[i].value.length > 20:
+				passwordStrong[i].style.backgroundColor = "green";
+			case password[i].value.length > 12:
+				passwordMedium[i].style.backgroundColor = "orange";
+			case password[i].value.length > 3:
+				passwordWeak[i].style.backgroundColor = "red";
+		}
+	});
 }
+
+form.addEventListener("submit", async (event) => {
+	event.preventDefault();
+	const formData = new FormData(form);
+	const info = Object.fromEntries(formData.entries());
+	try {
+		await axios
+			.get(`http://0.0.0.0:3000/users?email=${info.email}`)
+			.then(async (response) => {
+				let data = response.data[0];
+				if (!data) {
+					alert("This user doesnt exist");
+				} else if (info.password !== info.password2) {
+					alert("Passwords do not match");
+				} else {
+					const Object = { password: info.password };
+					await axios.patch(
+						`http://0.0.0.0:3000/users?email=${info.email}`,
+						Object
+					);
+				}
+			});
+	} catch {}
+});
